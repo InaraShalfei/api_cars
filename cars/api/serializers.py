3090f85845f5fab1804c20b3d077573348e9f070
InaraShalfei <inarashugaikina@gmail.com>
2023-03-21 18:27:14 +0600

    Set up JWT-auth

--- cars/api/serializers.py
+++ cars/api/serializers.py
@@ -1,6 +1,13 @@
 from rest_framework import serializers
 
 from .models import Auto, Owner
+from djoser.serializers import UserSerializer, User
+
+
+class CustomUserSerializer(UserSerializer):
+    class Meta:
+        model = User
+        fields = ('email', 'id', 'username', 'first_name', 'last_name')
 
 
 class OwnerSerializer(serializers.ModelSerializer):
--- cars/cars/settings.py
+++ cars/cars/settings.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'djoser',
     'api',
 ]
 
@@ -111,3 +113,19 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
+
+
+SIMPLE_JWT = {
+   'ACCESS_TOKEN_LIFETIME': timedelta(days=3),
+   'AUTH_HEADER_TYPES': ('Bearer',),
+}
