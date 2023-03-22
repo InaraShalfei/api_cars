c737c8e8c9f4b82dc12dea477a627af48710609c
InaraShalfei <inarashugaikina@gmail.com>
2023-03-22 21:30:15 +0600

    Create update method in Auto serializer

--- cars/api/serializers.py
+++ cars/api/serializers.py
@@ -33,25 +33,15 @@ class AutoSerializer(serializers.ModelSerializer):
                 owner=current, auto=auto)
         return auto
 
-    # def update(self, instance, validated_data):
-    #     owners = validated_data.pop('owners')
-    #     RecipeIngredient.objects.filter(recipe=instance).delete()
-    #     recipe_ingredients = {}
-    #     for item in ingredients:
-    #         amount = item.pop('amount')
-    #         ingredient = item.pop('id')
-    #         if ingredient.id not in recipe_ingredients:
-    #             recipe_ingredients[ingredient.id] = RecipeIngredient(
-    #                 amount=amount,
-    #                 ingredient=ingredient,
-    #                 recipe=instance)
-    #         else:
-    #             recipe_ingredients[ingredient.id].amount += amount
-    #     for recipe_ingredient in recipe_ingredients.values():
-    #         recipe_ingredient.save()
-    #     for (key, value) in validated_data.items():
-    #         setattr(instance, key, value)
-    #     instance.save()
-    #     instance.tags.set(tags)
-    #     return instance
+    def update(self, instance, validated_data):
+        owners = validated_data.pop('owners')
+        for owner in owners:
+            current, status = Owner.objects.get_or_create(
+                **owner)
+            OwnerAuto.objects.create(
+                owner=current, auto=instance)
+        for (key, value) in validated_data.items():
+            setattr(instance, key, value)
+        instance.save()
+        return instance
 
