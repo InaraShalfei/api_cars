621a50b1401564f4235f8fa364c5d25e7c7857ff
InaraShalfei <inarashugaikina@gmail.com>
2023-03-23 21:03:52 +0600

    Create additional endpoint for autos with more than 1 owner

--- cars/api/views.py
+++ cars/api/views.py
@@ -1,4 +1,7 @@
+from django.db.models import Count
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .models import Auto, Owner
 from .serializers import AutoSerializer, OwnerSerializer
@@ -7,6 +10,12 @@ from .serializers import AutoSerializer, OwnerSerializer
 class AutoViewsSet(viewsets.ModelViewSet):
     queryset = Auto.objects.all()
     serializer_class = AutoSerializer
+
+    @action(detail=False)
+    def multiple_purchase(self, request):
+        autos = Auto.objects.annotate(owners_count=Count('owners')).filter(owners_count__gte=1)
+        serializer = self.get_serializer(autos, many=True)
+        return Response(serializer.data)
 
 
 class OwnerViewSet(viewsets.ModelViewSet):
