1f203a3d10f26dc28cc751c6ac85016d5ac01ae0
InaraShalfei <inarashugaikina@gmail.com>
2023-03-26 13:27:06 +0600

    Add new fields (and field with choices) into Owner model

--- cars/api/models.py
+++ cars/api/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
+from .choices import CHOICES
+
 
 class Owner(models.Model):
     first_name = models.CharField(max_length=128)
     last_name = models.CharField(max_length=128)
+    date_of_birth = models.IntegerField(default=1990)
+    nationality = models.CharField(max_length=25, choices=CHOICES, default='Казахстан')
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
@@ -14,7 +18,7 @@ class Auto(models.Model):
     color = models.CharField(max_length=100)
     production_year = models.IntegerField(blank=True, null=True)
     engine_capacity = models.IntegerField(blank=True, null=True)
-    owners = models.ManyToManyField(Owner, through='OwnerAuto')
+    owners = models.ManyToManyField(Owner, through='OwnerAuto', related_name='autos')
     vin_code = models.IntegerField(unique=True)
 
     def __str__(self):
